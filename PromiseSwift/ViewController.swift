d2b5624b94e0d85615ef3796f1f720f965827be9
Petro Korienev <soxjke@gmail.com>
2016-03-07 16:45:34 +0200

    Initial Commit

---
+++ PromiseSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PromiseSwift
+//
+//  Created by Petro Korienev on 3/7/16.
+//  Copyright © 2016 Techery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
